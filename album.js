5ab1d5c3c3ca1f32741c7bd56a70b8747f272f04
FraLama99 <info@3dlama.it>
2025-01-07 22:44:20 +0100

    aggiornamento js

--- album.js
+++ album.js
@@ -20,25 +20,46 @@ const searchDeezer = (searchQuery) => {
 const popolaAlbum = (data) => {
     const album = document.getElementById('album');
     album.innerHTML = `
-        <div class="card">
-            <img src="${data.cover}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${data.title}</h5>
-                <p class="card-text">${data.artist.name}</p>
-                <p class="card-text">${data.release_date}</p>
-                <p class="card-text">${data.nb_tracks} tracks</p>
-                <div class="list-tracks">
-                ${data.tracks.data.map((track, index) => `
-                    <div class="track">
-                        <p>${index + 1}. ${track.title} - ${track.duration}s</p>
-                    </div>
-                `).join('')}
+        <div class="album-container d-flex flex-column">
+            <div class="d-flex flex-row align-items-center ">
+                <div class="album-cover">
+                    <img src="${data.cover}" alt="${data.title}">
+                </div>
+                <div class="album-info ps-4">
+                    <h1>${data.title}</h1>
+                        <div class="d-flex flex-row align-items-center">
+                            <img src="${data.artist.picture}" alt="${data.artist.name}" class="rounded-circle" style="width: 50px; height: 50px;">
+                            <h4>${data.artist.name}</h4>
+                            <div class="release-date ps-2">${data.release_date}</div>
+                            <div class="tracks-count ps-2">${data.nb_tracks} tracks</div>
+                        </div>
                 </div>
             </div>
+                <div class="tracks-list ">                    
+                        ${data.tracks.data.map((track, index) => `
+                    <div class=" mt-4 d-flex justify-content-between align-items-center">
+                            <div class="track-item d-flex flex-row gap-3 align-items-center">
+                                <div id="track-number ">${index + 1}</div>
+                                <div class="d-flex flex-column">
+                                    <div id="track-title">${track.title}</div>
+                                    <div id="track-artist" class="text-secondary" >${track.artist.name}</div>
+                                </div>
+                            </div>  
+                            <div id="track-rank">${track.rank}</div>
+                            <div id="track-duration">${durataFormattata(track.duration)}s</div>
+                        </div>
+                    `).join('')}
+                </div>
+            
         </div>
     `;
     console.log(data);
 };
+function durataFormattata(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
 
 const searchButton = document.getElementById('ricerca');
 if (searchButton) {
