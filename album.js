d55ef0700805259af6dd1a33e5aee00d768ab449
FraLama99 <info@3dlama.it>
2025-01-10 17:59:57 +0100

    js e html da verificare

--- album.js
+++ album.js
@@ -108,6 +108,7 @@ const popolaAlbum = (data) => {
                         </div>`;
     tracce = data.tracks.data;
     caricaCanzone();
+    caricaCanzoneCell();
     console.log(tracce);
     return tracce;
 };
@@ -118,7 +119,7 @@ function durataFormattata(seconds) {
     return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
 
-const searchButton = document.getElementById('ricerca');
+/* const searchButton = document.getElementById('ricerca');
 if (searchButton) {
     searchButton.addEventListener('click', () => {
         const searchInput = document.getElementById('search');
@@ -130,7 +131,7 @@ if (searchButton) {
     });
 } else {
     console.error('Search button not found');
-}
+} */
 searchDeezer('1121401');
 /* 1121401 */
 /* 216487 */
@@ -141,19 +142,56 @@ const caricaCanzone = () => {
     const tracks = document.querySelectorAll('.track-item');
     tracks.forEach((track, index) => {
         track.addEventListener('click', () => {
-            const player = document.getElementById('player');
+            const player = document.getElementById('player-album');
             if (player) {
                 player.innerHTML = `
-                    <div class="now-playing d-flex align-items-center gap-3">
-                        <img src="${tracce[index].album.cover_small}" alt="${tracce[index].title}" />
-                        <div>
-                            <div class="track-title fw-light">${tracce[index].title}</div>
-                            <div class="track-artist fw-light">${tracce[index].artist.name}</div>
+                        <div class="d-flex">
+                            <img src="${tracce[index].album.cover_small}" alt="${tracce[index].title}">
                         </div>
-                    </div>
+                        <div class="d-flex flex-column justify-content-center ps-3">
+                            <h6 class="m-0 col-12 d-flex">${tracce[index].title}</h6>
+                            <p class="m-0">${tracce[index].artist.name}</p>
+                        </div>
+                        <div class="d-flex align-items-center ps-4 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart"
+                            viewBox="0 0 16 16">
+                            <path
+                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                        </svg>
+                        </div>
+                    
                 `;
             }
             console.log('Playing:', tracce[index].title);
         });
     });
 };
+const caricaCanzoneCell = () => {
+    const tracks = document.querySelectorAll('.track-item');
+    tracks.forEach((track, index) => {
+        track.addEventListener('click', () => {
+            const player = document.getElementById('current-track');
+            if (player) {
+                player.innerHTML = `
+                        <div class="d-flex">
+                            <img src="${tracce[index].album.cover_small}" alt="${tracce[index].title}">
+                        </div>
+                        <div class="d-flex flex-column justify-content-center ps-3">
+                            <h6 class="m-0 col-12 d-flex">${tracce[index].title}</h6>
+                            <p class="m-0">${tracce[index].artist.name}</p>
+                        </div>
+                        <div class="d-flex align-items-center ps-4 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart"
+                            viewBox="0 0 16 16">
+                            <path
+                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                        </svg>
+                        </div>
+                    
+                `;
+            }
+            console.log('Playing:', tracce[index].title);
+        });
+    });
+};
+
